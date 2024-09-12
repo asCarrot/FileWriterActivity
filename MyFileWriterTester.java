f517556234ce98fbd744a9c057251705686b1588
Ace <jschneider1@hwemail.com>
2024-09-12 09:47:01 -0700

    Revert "Merge commit '5a2cdc0b6c263cbb6a85c3c91fe0a7b0889a1419' into HEAD"
    This reverts commit 68429b3ce127a0c5d3dde46dd7712dc572884d54, reversing
    changes made to 264dbb926f41be42b32a7099eba21144d3fa84d3.

--- MyFileWriterTester.java
+++
@@ -1,8 +0,0 @@
-public class MyFileWriterTester {
-    public static void main(String[] args) {
-        System.out.println("Testing generateSecretFile()...");
-        MyFileWriter.generateSecretFile();
-        MyFileWriter.generateRegularFile();
-        System.out.println("Should be a success!");
-    }
-}
